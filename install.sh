df4ca43e4b6eb0aa89fd7cc3aec86fdd87facecd
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-01 22:08:26 -0600

    add sudo where appropriate

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # preinstall
-rfkill unblock wifi
-ip link set wlan0 up
+sudo rfkill unblock wifi
+sudo ip link set wlan0 up
 echo "\n> scan wifi\n> services\n> agent on\n> connect wifi_NAME\n> quit"
-connmanctl
-yes | pacman -Sy --needed python
+sudo connmanctl
+yes | sudo pacman -Sy --needed python
 
 # install
-python src/install.py
-cp src/iamchroot.py /mnt/root/
+sudo python src/install.py
+sudo cp src/iamchroot.py /mnt/root/
 
 # chroot
-artix-chroot /mnt /bin/bash -c "python /root/iamchroot.py && exit"
+sudo artix-chroot /mnt /bin/bash -c "python /root/iamchroot.py && exit"
 
 # clean up
-umount -R /mnt
-cryptsetup close /dev/mapper/cryptroot
-cryptsetup close /dev/mapper/ctyptswap
+sudo umount -R /mnt
+sudo cryptsetup close /dev/mapper/cryptroot
+sudo cryptsetup close /dev/mapper/ctyptswap
 echo "You may now poweroff."
