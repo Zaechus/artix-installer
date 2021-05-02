8e070052b9173dce3aa66561e19c09ace2650bc6
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-02 16:01:18 -0600

    only use printf with single quotes

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # preinstall
 sudo rfkill unblock wifi
 sudo ip link set wlan0 up
-printf "\n> scan wifi\n> services\n> agent on\n> connect wifi_NAME\n> quit\n"
+printf '\n> scan wifi\n> services\n> agent on\n> connect wifi_NAME\n> quit\n'
 sudo connmanctl
 yes | sudo pacman -Sy --needed python
 
 # install
 sudo sfdisk -l
-printf "\nDisk to install to (e.g. /dev/sda): " && read MY_DISK && echo $MY_DISK
+printf '\nDisk to install to (e.g. /dev/sda): ' && read MY_DISK && echo $MY_DISK
 sudo python src/install.py $MY_DISK
 sudo cp src/iamchroot.py /mnt/root/
 
@@ -20,4 +20,4 @@ sudo artix-chroot /mnt /bin/bash -c "python /root/iamchroot.py $MY_DISK && exit"
 sudo umount -R /mnt
 sudo cryptsetup close /dev/mapper/cryptroot
 sudo cryptsetup close /dev/mapper/cryptswap
-printf "\nYou may now poweroff.\n"
+printf '\nYou may now poweroff.\n'
