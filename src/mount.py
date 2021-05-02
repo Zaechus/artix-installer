10aff3c07665018fbf017688396c1f1ed3d3acd9
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-02 04:32:08 -0600

    improve mount.py

--- src/mount.py
+++ src/mount.py
@@ -4,23 +4,18 @@ import sys
 
 from subprocess import run
 
-disk = ""
-while True:
-    while True:
-        run("fdisk -l", shell=True)
-        disk = input("\nDisk to mount (e.g. `/dev/sda`): ").strip()
-        if len(disk) > 0:
-            break
-    break
+disk = sys.argv[1]
 
-# Setup encrypted partitions
+run("sudo umount -R /mnt", shell=True)
+run("sudo cryptsetup close /dev/mapper/cryptswap", shell=True)
+run("sudo cryptsetup close /dev/mapper/cryptroot", shell=True)
+
 cryptpass = input("Encryption password: ").strip()
 
-run(f"yes '{cryptpass}' | cryptsetup open {disk}3 cryptroot", shell=True)
-run(f"yes '{cryptpass}' | cryptsetup open {disk}2 cryptswap", shell=True)
+run(f"yes '{cryptpass}' | sudo cryptsetup open {disk}3 cryptroot", shell=True)
+run(f"yes '{cryptpass}' | sudo cryptsetup open {disk}2 cryptswap", shell=True)
 
-# Mount subvolumes and boot
-run("mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt", shell=True)
-run("mount -o compress=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots", shell=True)
-run("mount -o compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home", shell=True)
-run(f"mount {disk}1 /mnt/boot/efi", shell=True)
+run("sudo mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt", shell=True)
+run("sudo mount -o compress=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots", shell=True)
+run("sudo mount -o compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home", shell=True)
+run(f"sudo mount {disk}1 /mnt/boot/efi", shell=True)
