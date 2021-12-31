cc39932e8142b08f1353b6d63c0f20c1f2f28bcb
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-12-31 13:38:14 -0700

    messy commit; begin rewrite to bash

--- install.sh
+++ install.sh
@@ -1,23 +1,94 @@
 #!/bin/sh
 
-# preinstall
-yes | sudo pacman -Sy --needed python
+confirm_password () {
+    local pass1=0
+    local pass2=1
+    until [[ $pass1 == $pass2 ]]; do
+        printf "$1: " && read -rs $pass1
+        printf "confirm $1: " && read -rs $pass2
+    done
+    return $pass2
+}
 
-# install
+# Check boot mode
+[[ ! -d /sys/firmware/efi ]] && printf "Not booted in UEFI mode. Aborting..."; exit 1
+
+# Load keymap
+loadkeys us
+
+# Choose disk
 while :
 do
     sudo fdisk -l
-    printf '\nDisk to install to (e.g. /dev/sda): ' && read MY_DISK
-    if test -b "$MY_DISK"; then
-        echo $MY_DISK
-        break
-    fi
+    printf "\nDisk to install to (e.g. /dev/sda): " && read my_disk
+    [[ -b $my_disk ]] && break
 done
 
-sudo python src/install.py $MY_DISK
-sudo cp src/iamchroot.py /mnt/root/
+part1="$my_disk"1
+part2="$my_disk"2
+part3="$my_disk"3
+if [[ "nvme" == *"$my_disk"* ]]; then
+    part1="$my_disk"p1
+    part2="$my_disk"p2
+    part3="$my_disk"p3
+fi
 
-# chroot
-sudo artix-chroot /mnt /bin/bash -c "python /root/iamchroot.py $MY_DISK && exit"
+# Swap size
+printf "Size of swap partition in GiB (4): " && read swap_size
+[[ ! -z swap_size ]] && swap_size=4
+
+# Choose filesystem
+until [[ $my_fs == "btrfs" || $my_fs == "ext4" ]]; do
+    printf "Filesystem (btrfs/ext4): " && read my_fs
+done
+
+root_part=$part3
+[[ $my_fs == "ext4" ]] && root_part=$part2
+
+# Encrypt or not
+encrypted="y"
+printf "Encrypt? (Y/n): " && read encrypted
+
+my_root="/dev/mapper/root"
+my_swap="/dev/mapper/swap"
+if [[ $encrypted == "n" ]]; then
+    my_root=$part3
+    my_swap=$part2
+    [[ $my_fs == "ext4" ]] && my_root=$part2 && my_swap="/dev/MyVolGrp/swap"
+else
+    cryptpass=$(confirm_password "encryption password")
+fi
+
+# Timezone
+printf "Region/City (e.g. 'America/Denver'): " && read region_city
+[[ ! -z region_city ]] && region_city="America/Denver"
+
+# Host
+my_hostname=$(confirm_password "hostname")
+
+# Microcode
+printf "Microcode (intel/amd/both):" && read ucode
+case ucode in
+    intel)
+        ucode="intel-ucode"
+    amd)
+        ucode="amd-ucode"
+    *)
+        ucode="intel-ucode amd-ucode"
+        ;;
+esac
+
+# Users
+root_password=$(confirm_password "root password")
+
+my_username=$(confirm_password "username")
+user_password=$(confirm_password "user password")
+
+# Install
+sudo sh src/installer.sh
+
+# Chroot
+sudo cp src/iamchroot.sh /mnt/root/
+sudo artix-chroot /mnt /bin/bash -c 'sh /root/iamchroot.sh; rm /root/iamchroot.sh; exit'
 
 printf '\n`sudo artix-chroot /mnt /bin/bash` back into the system to make any final changes.\n\nYou may now poweroff.\n'
---
+++ src/iamchroot.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# Boring stuff you should probably do
+ln -sf /usr/share/zoneinfo/$region_city /etc/localtime
+hwclock --systohc
+
+# Localization
+sed -i '/en_US\.UTF-8/s/^#//g' /etc/locale.gen
+locale-gen
+printf "LANG=en_US.UTF-8\n" > /etc/locale.conf
+printf "KEYMAP=us\n" > /etc/vconsole.conf
+
+# Host stuff
+printf "$my_hostname\n" > /etc/hostname
+printf "hostname=\"$hostname\"\n" > /etc/conf.d/hostname
+printf "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$hostname.localdomain\t$hostname\n" > /etc/hosts
+
+# Install boot loader
+root_part_uuid=$(blkid $root_part -o value -s UUID)
+
+root_flags="cryptdevice=UUID=$root_part_uuid:cryptroot root=$my_root"
+
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT=\"$root_flags\"/g' /etc/default/grub
+printf "\n\nGRUB_ENABLE_CRYPTODISK=y\n" >> /etc/default/grub
+
+grub-install --target=x86_64-efi --efi-directory=/boot --recheck
+grub-install --target=x86_64-efi --efi-directory=/boot --removable --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Local.start
+printf "rfkill unblock wifi\n" > /etc/local.d/local.start
+chmod +x /etc/local.d/local.start
+
+# Add default user
+yes $root_password | passwd
+
+rm /etc/skel/.bash*
+useradd -D -s /bin/zsh
+
+useradd -m $username
+
+yes $user_password | passwd $username
+usermod -a -G wheel $username
+usermod -a -G video $username
+usermod -a -G audio $username
+
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
+
+# Other stuff you should do
+rc-update add connmand default
+
+printf "\n$my_swap\t\tswap\t\tswap\t\tsw\t0 0\n" >> /etc/fstab
+if [[ $encrypted != "n" && my_fs == "btrfs" ]]; then
+    swap_uuid=$(blkid $part2 -o value -s UUID)
+
+    printf 'run_hook() {{\n\tcryptsetup open /dev/disk/by-uuid/{swap_uuid} cryptswap\n}}\n' > /etc/initcpio/hooks/openswap
+    printf 'build() {\n\tadd_runscript\n}\n' > /etc/initcpio/install/openswap
+fi
+
+# Configure mkinitcpio
+if [[ $my_fs == "ext4" ]]; then
+    sed -i 's/^HOOKS.*$/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt lvm2 filesystems fsck)/g' /etc/mkinitcpio.conf
+else if [[ $my_fs == "btrfs" ]]; then
+    sed -i 's/^HOOKS.*$/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt openswap filesystems fsck)/g' /etc/mkinitcpio.conf
+    sed -i 's/BINARIES=()/BINARIES=(\/usr\/bin\/btrfs)/g' /etc/mkinitcpio.conf
+fi
+
+mkinitcpio -P
